12bc03da38438925145c82fb03a34744d1ddd017
eter2 <eter2hy@gmail.com>
2021-01-26 18:10:15 +0900

    Add

---
+++ baekjoon/C++/2557.cpp
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/2557
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
---
+++ baekjoon/JAVA/2557.java
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/2557
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
